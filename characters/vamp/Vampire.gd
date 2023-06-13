867d0c13f33bd8f59341c7db35608e4e75abb959
Daniel Agbay <101584191+dagbay-rh@users.noreply.github.com>
2023-06-13 17:13:16 -0500

    Movement stuff (#12)
    some basic movement with gravity pulled from the default godot movement
    script
    
    some stuff to note:
    
    - I moved the vampire movement to the vampire scene, so afaik we should
    be able to move this scene around and the movement goes with it
    - I named some of the 2D physics layers which you can see in
    project.godot layer 1=walls, 2=player, 3=enemies
    - the gravity value is a global setting, so we can pull that anywhere
    else we need to sync gravity with

--- characters/vamp/Vampire.gd
+++ characters/vamp/Vampire.gd
@@ -1,18 +1,26 @@
 extends CharacterBody2D
 
-@export var move_speed : float = 100
+const SPEED = 100.0
+const JUMP_VELOCITY = -300.0
 
-func _physics_process(_delta):
-	var input_direction = Vector2(
-		
-		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		Input.get_action_strength("down") - Input.get_action_strength("up")
-	)
-	print(input_direction)
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-	velocity = input_direction * move_speed
-	gravity += 30
-	if Input.is_action_just_pressed("space"):   
-	   direction.y = -700
-	direction.y = gravity
-	move_and_slide(direction, Vector2.UP)
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()

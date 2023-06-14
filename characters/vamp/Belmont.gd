840745fde7041814559fd7c804877faa61ed3210
Stephen Adams <stephen.adams@redhat.com>
2023-06-14 10:27:48 -0400

    Improved vampire animation
    Signed-off-by: Stephen Adams <stephen.adams@redhat.com>

--- characters/vamp/Belmont.gd
+++ characters/vamp/Belmont.gd
@@ -1,48 +1,94 @@
 extends CharacterBody2D
 
 
-const SPEED = 200.0
-const JUMP_VELOCITY = -400.0
+@export var speed : float = 200.0
+@export var jump_velocity : float = -400.0
+
+@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
+@onready var body_collision : CollisionShape2D = $bodyCollision
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var animation_locked : bool = false
+var direction : Vector2 = Vector2.ZERO
 
 func _ready():
 	pass
 
 func _physics_process(delta):
 	
-	$AnimatedSprite2D.connect("animation_finished", on_animated_sprite_2d_finished)
-	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		$AnimatedSprite2D.play("jump_flip")
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			jump()
+	
+	if Input.is_action_pressed("down"):
+		if is_on_floor():
+			crouch()
+	
+	if Input.is_action_just_released("down"):
+		if animated_sprite.animation == "crouch":
+			animation_locked = false
+			animated_sprite.stop()
+			animated_sprite.emit_signal("animation_finished")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	if Input.is_action_pressed("left") and is_on_floor():
-		$AnimatedSprite2D.set_flip_h(true)
-		$AnimatedSprite2D.play("running")
+
 		
-	if Input.is_action_pressed("right") and is_on_floor():
-		$AnimatedSprite2D.set_flip_h(false)
-		$AnimatedSprite2D.play("running")
-		
-	if Input.is_action_pressed("down") and is_on_floor():
-		$AnimatedSprite2D.play("crouch")
-		
-	var direction = Input.get_axis("left", "right")
+	direction = Input.get_vector("left", "right", "up", "down")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction.x * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
+	update_animation()
+	update_facing_direction()
 
-func on_animated_sprite_2d_finished():
-	$AnimatedSprite2D.play("default")
+func update_animation():
+	if not animation_locked:
+		if not is_on_floor():
+			animated_sprite.play("jump_flip")
+		else:
+			if direction.x != 0:
+				animated_sprite.play("running")
+			else:
+				animated_sprite.play("default")
+		
+func update_facing_direction():
+	if direction.x > 0:
+		animated_sprite.flip_h = false
+	elif direction.x < 0:
+		animated_sprite.flip_h = true
+
+func jump():
+	velocity.y = jump_velocity
+	animated_sprite.play("jump_flip")
+	animation_locked = true
+
+func crouch():
+	if direction.x == 0:
+		update_collision_shape()
+		animated_sprite.play("crouch")
+		animation_locked = true
+		
+func update_collision_shape():
+	if animated_sprite.animation == "crouch":
+		body_collision.shape.height = 18
+		body_collision.position.y = 8
+
+
+func reset_collision_shape():
+		body_collision.shape.height = 30
+		body_collision.position.y = 2.5
+
+
+func _on_animated_sprite_2d_finished():
+	if(animated_sprite.animation == "jump_flip"):
+		animation_locked = false
+	if(animated_sprite.animation == "crouch"):
+		reset_collision_shape()
+		animation_locked = false

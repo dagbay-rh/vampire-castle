466c56539a92e280561cd7dfb20e83ff8eb350c3
Stephen Adams <stephen.adams@redhat.com>
2023-06-15 15:09:57 -0400

    Wire up the health bar
    Signed-off-by: Stephen Adams <stephen.adams@redhat.com>

--- ui/scripts/bars.gd
+++ ui/scripts/bars.gd
@@ -1,11 +1,13 @@
 extends Control
 
 @onready var special : TextureProgressBar = $Special
+@onready var health : TextureProgressBar = $Health
 @onready var animation_player : AnimationPlayer = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	special.connect("max_reached", _on_special_max_reached)
+	PlayerState.connect("hurt", _on_player_take_damage)
 	pass # Replace with function body.
 
 
@@ -13,5 +15,11 @@ func _ready():
 func _process(delta):
 	pass
 
-func _on_special_max_reached():
+func _on_special_max_reached(_value):
 	animation_player.play("special_ready")
+
+func _on_player_take_damage(amount):
+	health.value = health.value - amount
+	# need to wire this up to death
+	if health.value <= 0:
+		print("game over, man")

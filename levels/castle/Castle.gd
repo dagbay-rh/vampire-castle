3d334dfea6c4035aac3bfa0b81996b0ecf33accc
Stephen <stephen.adams@redhat.com>
2023-06-15 20:32:29 -0400

    Modify how we set music
    For Castle, I added an export var that accepts an audio stream. Probably going to do this for other scenes as we get them. That way we can drag in new music we want instead of having to code it all. Efficiency ++

--- levels/castle/Castle.gd
+++ levels/castle/Castle.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
-@export var is_playable = true
+@export var is_playable : bool = true
+@export var level_music : AudioStream = load("res://art/music/01 - DavidKBD - Belmont Chronicles Pack - Belmont Chronicles.ogg")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	MusicPlayer.change_music_now(level_music)
+

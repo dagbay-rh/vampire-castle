d274e079e4ba0d38baa7810b383b4641731663f3
Stephen <stephen.adams@redhat.com>
2023-06-17 21:59:11 -0400

    Add win state end credits

--- levels/BelmontBedroom/belmont_bedroom.gd
+++ levels/BelmontBedroom/belmont_bedroom.gd
@@ -9,3 +9,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func end_game():
+	SceneSwitch.switch_scene("res://ui/win.tscn")
